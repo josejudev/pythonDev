b108ada5225722be9cf3a7a20d04bb4760feb1da
JoseJu <joseju@mail.com>
2022-07-13 23:26:41 -0500

    latest changes

--- getFiles.py
+++ getFiles.py
@@ -4,33 +4,41 @@ from datetime import datetime
 
 def new_files():
     #! =>Comprobar si existen archivos
-    bakFile = glob.glob("/home/joseju/Documentos/files_backup/*.pdf")
-    if bakFile:
-        print("\n ✖ Files found")
+    bakFile = os.listdir("/home/joseju/Documentos/files_backup")
+    # if bakFile:
+    #     print("\n ✖ Files found")
         
-        
-    for files in bakFile:
-        try:
-            print("\t Archivo: → ",files)
-        except OSError as e:
-            print(f"Error:{ e.strerror}")
+    # filesArray = []
+    # if bakFile:
+    #     print("\nFound {} folders".format(len(bakFile)))
+    #     for file in bakFile:
+    #         filesArray.append(file)
+    
+    # else:
+    #     print("No files found")
+
+    # folder = []
+    # for ar in filesArray:
+    #     folder.append(ar.split('/')[-2]) 
 
     #! =>crear la carpeta con la fecha actual
-    if len(bakFile) > 0:
-                folderName = datetime.today().strftime('%Y-%m-%d')
-                fileDir = f"/home/joseju/Documentos/files_backup/{folderName}"
-                if not os.path.exists(fileDir):
-                    os.makedirs(fileDir)
-    time.sleep(1) 
+    # if len(bakFile) > 0:
+    #             folderName = folder[0]
+    #             fileDir = f"/home/joseju/Documentos/files_backup/{folderName}"
+    #             if not os.path.exists(fileDir):
+    #                 os.makedirs(fileDir)
+    # time.sleep(1) 
+
+    folderName = bakFile[0]
 
     #! =>comprimiendo archivos
     zipFolder = zipfile.ZipFile(f'/home/joseju/Documentos/files_backup/{folderName}/{folderName}.zip', 'w')
     path = f'/home/joseju/Documentos/files_backup/{folderName}/{folderName}.zip'
 
-    for folders, subfolders, files in os.walk(f'/home/joseju/Documentos/files_backup/'):
+    for folders, subfolders, files in os.walk(f'/home/joseju/Documentos/files_backup/{folderName}'):
         for file in files:
             if file.endswith('.pdf'):
-                zipFolder.write(os.path.join(folders, file), os.path.relpath(os.path.join(folders, file), '/home/joseju/Documentos/files_backup/'))
+                zipFolder.write(os.path.join(folders, file), os.path.relpath(os.path.join(folders, file), f'/home/joseju/Documentos/files_backup/{folderName}'))
 
     zipFolder.close()
     print("\n ✓ Files compressed successfully")
